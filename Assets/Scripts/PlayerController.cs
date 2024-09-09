87d5aad987a9d6cadf80ec46af76adce5c70a85b
Oscar Devs <ohernandezba@gmail.com>
2024-09-09 22:21:41 +0200

    Ajustes PostProcessing y Player Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,24 +15,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool isMoving;
 
     void Start()
-    {
-        //Move(new Vector3(0, 0, 2), 1);
-        //Move(new Vector3(-2, 0, 2), 2);
-        //Move(new Vector3(-2, 0, 4), 3);
-        //Move(new Vector3(0, 0, 4), 4);
-        StartCoroutine(Test());
-    }
-
-    IEnumerator Test()
-    {
-        yield return new WaitForSeconds(1);
-        MoveLeft();
-        yield return new WaitForSeconds(2);
-        MoveRight();
-        yield return new WaitForSeconds(2);
-        MoveForward();
-        yield return new WaitForSeconds(2);
-        MoveBackward();
+    {        
     }
 
     private void Move(Vector3 destinationPosition, float delay = 0.25f)
